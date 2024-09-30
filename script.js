70168f665b9a9eebfd3f7cdc3c2d120f759c4b81
Focal-textil <focalti@gmail.com>
2024-09-30 17:01:07 -0300

    updated

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRht2SMUJJcXy4WTZbwgJ12nLTm5XQ5E7DuwKps2g2rRd_0Nvo9xsw8QdKdlWr162VGhrscqt7k7PHg/pub?output=csv";
 
 
+
 function load_data(){
     fetch(url)
     .then(response => response.text())
@@ -12,13 +13,23 @@ function load_data(){
             dynamicTyping: true,
         });
 
+        // ------------TIPOS------------------
         const data = resultados.data;
+        let tipo = data.map(item => item.Tipo);
+        tipo = [...new Set(tipo)];
 
-        let diversos = data.filter(item => item.Tipo === "DIVERSOS");
+        const selectbox = document.getElementById("diversos");
+        
+        tipo.forEach(valor => {
+            const option = document.createElement('option');
+            option.value = valor;
+            option.text = valor;   // Define o texto visível da opção
+            selectbox.appendChild(option);  // Adiciona a opção ao select
+          });
 
-        // console.log(data);
-        // console.log(diversos);
+        console.log(tipo);
 
+        // -----------------------------------
     })
 }
 
